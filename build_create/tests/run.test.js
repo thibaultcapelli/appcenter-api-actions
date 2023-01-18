77142b18cf258ca2d80376f074844d9ad2f33b4b
Thibault Capelli <thibault.capelli@tohero.fr>
2023-01-18 20:37:52 +0100

    chore: remove js file autogenrated by IDE

--- build_create/tests/run.test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-test('run successfully', async () => {
-    // await expect(run({ name: 'foo' })).resolves.toBeUndefined()
-});
