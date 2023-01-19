14f83b4638194ca1ede24bd07715c3897df72f18
Thibault Capelli <53807506+thibaultcapelli@users.noreply.github.com>
2023-01-19 18:01:53 +0100

    Update main.ts

--- build_get-branch-config/src/main.ts
+++ build_get-branch-config/src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import { run } from './run'
 
-const main = async (): Promise<void> => {
+const main = async () => {
   return await run({
     apiToken: core.getInput('API_TOKEN', { required: true }),
     ownerName: core.getInput('OWNER_NAME', { required: true }),
