464bd0bdaa526a16c18de3a4555b46a3bb60dc3d
Thibault Capelli <53807506+thibaultcapelli@users.noreply.github.com>
2023-01-19 14:41:23 +0100

    Update run.test.ts

--- build_update-branch-config/tests/run.test.ts
+++ build_update-branch-config/tests/run.test.ts
@@ -10,6 +10,8 @@ const appName = process.env.APP_NAME as string;
 const branch = process.env.BRANCH as string;
 const config = JSON.parse(process.env.CONFIG as string) as object;
 
+jest.setTimeout(10000);
+
 beforeEach(async () => {
     // TODO: Create a configuration for the given branch to update it.
 });
